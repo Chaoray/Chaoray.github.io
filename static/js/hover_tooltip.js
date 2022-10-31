30247f454467bb2073d41f419f89638c359dbd93
Hino Shimizu <65358638+Chaoray@users.noreply.github.com>
2022-10-31 18:57:24 +0800

    我不知道做了啥

--- static/js/hover_tooltip.js
+++ static/js/hover_tooltip.js
@@ -1,7 +1,15 @@
 let tips = document.getElementsByTagName('tips');
+// Object.prototype.offset = (type = 'all') => {
+//     let e = this; let d = { height: this.offsetHeight, width: this.offsetWidth, top: 0, left: 0 };
+//     while (e !== document.body && e.offsetParent) {
+//         d.left += e.offsetLeft;
+//         d.top += e.offsetTop;
+//         e = e.offsetParent;
+//     }
+//     return (type in d ? d[type] : d);
+// }
 
-for (let i = 0; i < tips.length; i++) {
-    let tip = tips[i];
+for (let tip of tips) {
     tip.classList.add('tooltip');
 
     let span = document.createElement('span');
@@ -16,10 +24,21 @@ for (let i = 0; i < tips.length; i++) {
 
     tip.addEventListener('mouseover', (e) => {
         e.stopPropagation();
-        e.target.toggleAttribute('hovered');
+        tip.setAttribute('hovered', '');
     });
     tip.addEventListener('mouseleave', (e) => {
         e.stopPropagation();
-        e.target.toggleAttribute('hovered');
+        tip.removeAttribute('hovered');
     });
 }
+
+let html = document.querySelector('html');
+setTimeout(() => {//這樣
+    for (let tip of tips) {
+        let tipText = tip.querySelector('.tooltip-text');
+        console.log(tipText.getBoundingClientRect().top - html.scrollTop);
+        // 測試捲動後是否為定值..............
+        tipText.style.top = `calc(${tipText.getBoundingClientRect().top - 17/* tip高 */ - html.scrollTop}px - 0.4em)`;
+        tipText.style.left = `${tipText.getBoundingClientRect().left - html.scrollLeft}px`;
+    }
+}, 1e3);
